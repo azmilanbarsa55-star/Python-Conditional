56daedb76d4681696e81e9d4091f74b55f3381ee
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 15:31:33 +0500

    ifelse02.py bajarildi

--- ifelse02.py
+++ ifelse02.py
@@ -1,1 +1,21 @@
+a = float(input("birinchi sonni kiriting: "))
+b = float(input("ikkinchi sonni kiriting: "))
+amal = input("amalni kiriting: ")
 
+if amal == "+":
+    print(a + b)
+
+elif amal == "-":
+    print(a - b)
+
+elif amal == "*":
+    print(a * b)
+
+elif amal == "/":
+    if b == 0:
+        print("nolga bo'lib bo'lmaydi")
+    else:
+        print(a / b)
+
+else:
+    print("Noto'g'ri amal. Faqat +, -, *, / ishlatiladi.")
