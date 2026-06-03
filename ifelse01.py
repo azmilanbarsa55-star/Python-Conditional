184a7a22bdae34f71b8b28ef59b36fd5d3e6a0a6
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 15:10:33 +0500

    ifelse01.py bajarildi

--- ifelse01.py
+++ ifelse01.py
@@ -1,1 +1,16 @@
+ball = int(input("Ballni kiriting (0-100): "))
 
+if ball < 0:
+    print("Ball 0-100 oralig'ida bo'lishi kerak!")
+if ball > 100:
+    print("Ball 0-100 oralig'ida bo'lishi kerak!")
+elif 90 <= ball <= 100:
+    print("A (A'lo)")
+elif 80 <= ball <= 89:
+    print("B (Yaxshi)")
+elif 70 <= ball <= 79:
+    print("C (Qoniqarli)")
+elif 60 <= ball <= 69:
+    print("D (Qoniqarsiz)")
+else:
+    print("F (Rad)")
