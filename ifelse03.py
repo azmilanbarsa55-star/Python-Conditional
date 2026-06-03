9fc2b7586fc70c8479aa818f8005532f0e13997d
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 15:41:35 +0500

    ifelse03.py bajarildi

--- ifelse03.py
+++ ifelse03.py
@@ -1,1 +1,14 @@
 
+soat = input("soatni kiriting: ")
+if 5 < soat > 11:
+    print("Ertalab")
+elif 12 < soat >17:
+    print("Kunduzi")
+elif 18 < soat > 21:
+    print("kechqurun")
+elif 22 < soat > 23:
+    print("tun")
+elif 0 < soat > 4:
+    print("tun")
+else:
+    print("Soat 0-23 oralig'ida bo'lishi kerak!")
