928a57017e9efde84ba3df5b3584ae573922289b
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 15:45:43 +0500

    ifelse04.py bajarildi

--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,12 @@
 
+masofa = input("masofani kiriting: ")
+if 0 < masofa > 1:
+    print("Piyoda yuring")
+elif 1 < masofa > 5:
+    print("Velosiped yoki elektr skuter")
+elif 5 < masofa > 50:
+    print("Avtobus yoki mashina")
+elif masofa < 50:
+    print("Poyezd yoki samolyot")
+else:
+    print("Masofa manfiy bo'la olmaydi!")
