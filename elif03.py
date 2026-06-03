ca0e3996203d7ba3002119c97c4ff24480252de2
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 14:12:49 +0500

    elif03.py bajarildi

--- elif03.py
+++ elif03.py
@@ -1,1 +1,7 @@
-
+son = int(input("son kiriting: "))
+if son % 2 == 0:
+    print("son 2 ga bo'linadi")
+if son % 3 == 0:
+    print("son 3 ga bo'linadi")
+if son % 5 == 0:
+    print("son 5 ga bo'linadi")
