4ade9ccec20960dde21958d6b27f5539ba2dc30a
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 14:46:56 +0500

    elif05.py bajarildi

--- elif05.py
+++ elif05.py
@@ -1,1 +1,7 @@
-
+ob_havo = int(input("ob-havoni kiriting: "))
+if ob_havo < 0:
+    print("Juda sovuq! Issiq kiyim kiying.")
+if 0<= ob_havo <14:
+    print("Sovuq. Kurtka kiying.")
+if ob_havo > 15:
+    print("Ob-havo yaxshi.")
