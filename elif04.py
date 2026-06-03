2973621c2cadd603215518086931b8828078b30e
azmilanbarsa55-star <azmilanbarsa55@gmail.com>
2026-06-03 14:41:19 +0500

    elif04.py bajarildi

--- elif04.py
+++ elif04.py
@@ -1,1 +1,11 @@
+yosh = int(input("yoshingizni kiriting: "))
+narx = 100
 
+if yosh < 7:
+ print(int(narx - (narx * 50 / 100)))
+
+if 7 <= yosh <= 17:
+ print(int(narx - (narx * 30 / 100)))
+
+if yosh > 60:
+ print(int(narx - (narx * 20 / 100)))
